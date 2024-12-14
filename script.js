3cb13bbadd2cb708d45909cdf4e1569daf77d8a9
NagiFish <94735845+nezikog@users.noreply.github.com>
2024-12-14 14:21:56 +0300

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const inp = document.querySelector(".inp")
+const btn = document.querySelector(".btn");
+const alert = document.querySelector(".alert");
 
+let click = 0;
+
+btn.addEventListener('click', ()=>{
+    alert.innerHTML = inp.value
+})
