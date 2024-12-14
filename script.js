19676405ab3a2dded0f201e1275035158657c743
NagiFish <94735845+nezikog@users.noreply.github.com>
2024-12-14 14:45:28 +0300

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-const inp = document.querySelector(".inp")
-const btn = document.querySelector(".btn");
-const img = document.querySelector("img")
-// const alert = document.querySelector(".alert");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <!-- <h2>Введите пароль</h2>
+    <input class="inp" type="text"><br>
+    <button class="btn">Войти</button><br>
 
-// let click = 0;
+    <div class="alert">
 
-// btn.addEventListener('click', ()=>{
-//     alert.innerHTML = inp.value
-// })
+    </div> -->
+<!-- //************************************ -->
 
-// ***********************************************
+    <img class="img" src="https://avatars.dzeninfra.ru/get-zen_doc/42056/pub_62f0e680bcb361250b4c5311_62f0e79ebbe50c4d0fc77e80/scale_1200" alt="">
 
-btn.addEventListener('click', ()=>{
-    alert(inp.value*20 + "px")
-    img.style.width = (inp.value*20) + "px"
-})
+    <input class="inp" type="range">
+
+    <button class="btn">Подтвердить</button>
+
+    <h2 class="h2"></h2>
+    <script src="./script.js"></script>
+</body>
+</html>
