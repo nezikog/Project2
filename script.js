61dbaa1c10d298e1b26e60e5ff094c971c1c26a4
NagiFish <94735845+nezikog@users.noreply.github.com>
2024-12-14 14:36:50 +0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const inp = document.querySelector(".inp")
 const btn = document.querySelector(".btn");
-const alert = document.querySelector(".alert");
+const img = document.querySelector("img")
+// const alert = document.querySelector(".alert");
 
-let click = 0;
+// let click = 0;
+
+// btn.addEventListener('click', ()=>{
+//     alert.innerHTML = inp.value
+// })
+
+// ***********************************************
 
 btn.addEventListener('click', ()=>{
-    alert.innerHTML = inp.value
+    alert(inp.value*20 + "px")
+    img.style.width = (inp.value*20) + "px"
 })
